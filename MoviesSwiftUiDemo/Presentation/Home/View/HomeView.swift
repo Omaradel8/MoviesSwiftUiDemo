6ba6cd18f2b2b5e253ef3a4d5ced4c9fa4054beb
Omar Adel <omar.adel@softxpert.com>
2025-08-16 16:23:40 +0300

    rename ContentView to HomeView and add it's structure

---
+++ MoviesSwiftUiDemo/Presentation/Home/View/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  MoviesSwiftUiDemo
+//
+//  Created by Omar Adel on 16/08/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ScrollView {
+            ScrollView(.horizontal) {
+                HStack {
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
