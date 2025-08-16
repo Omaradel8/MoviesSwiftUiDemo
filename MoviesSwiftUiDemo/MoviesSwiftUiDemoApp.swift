3ee37912c396824a36bdb08da67d96e99c332526
Omar Adel <omar.adel@softxpert.com>
2025-08-16 15:17:22 +0300

    inital setup

---
+++ MoviesSwiftUiDemo/MoviesSwiftUiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesSwiftUiDemoApp.swift
+//  MoviesSwiftUiDemo
+//
+//  Created by Omar Adel on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesSwiftUiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
