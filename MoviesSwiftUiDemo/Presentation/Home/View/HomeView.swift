dde0308eeceace3c04e6885d56bd910df443b974
Omar Adel <omar.adel@softxpert.com>
2025-08-16 17:06:30 +0300

    add search bar to the screen

--- MoviesSwiftUiDemo/Presentation/Home/View/HomeView.swift
+++ MoviesSwiftUiDemo/Presentation/Home/View/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct HomeView: View {
     
     @State private var selectedIndex: Int = 0
+    @State private var searchText: String = ""
     
     let columns = [
         GridItem(.flexible(), spacing: 16),
@@ -18,9 +19,16 @@ struct HomeView: View {
     
     var body: some View {
         ScrollView {
+            
+            TextField("Search TMDB", text: $searchText)
+                .padding(10)
+                .background(Color(.systemGray6))
+                .cornerRadius(10)
+                .padding(.horizontal)
+            
             GenreListView(selectedIndex: $selectedIndex)
             
-            LazyVGrid(columns: columns, spacing: 16) {
+            LazyVGrid(columns: columns, spacing: 10) {
                 ForEach(HomeConstants.films, id: \.0) { title, year, imageName in
                     FilmCardView(imageName: imageName, filmTitle: title, fileReleaseYear: year)
                 }
