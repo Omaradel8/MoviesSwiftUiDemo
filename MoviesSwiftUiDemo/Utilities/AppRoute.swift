79bf176a4c1fc9b9b7f4dddc93054a4d8ceacb9b
Omar Adel <omar.adel@softxpert.com>
2025-08-16 18:14:24 +0300

    Create AppRoute enum that hold the destinations

---
+++ MoviesSwiftUiDemo/Utilities/AppRoute.swift
@@ -0,0 +1,13 @@
+//
+//  AppRoute.swift
+//  MoviesSwiftUiDemo
+//
+//  Created by Omar Adel on 16/08/2025.
+//
+
+import Foundation
+
+enum AppRoute: Hashable {
+    case HOME
+    case MOVIEDETAILS
+}
