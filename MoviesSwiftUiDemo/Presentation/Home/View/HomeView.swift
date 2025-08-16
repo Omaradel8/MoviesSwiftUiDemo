37c25e947c75b5f9b078ba8563985a2d4738ed3d
Omar Adel <omar.adel@softxpert.com>
2025-08-16 17:00:19 +0300

    add lazyVGrid of films

--- MoviesSwiftUiDemo/Presentation/Home/View/HomeView.swift
+++ MoviesSwiftUiDemo/Presentation/Home/View/HomeView.swift
@@ -11,9 +11,21 @@ struct HomeView: View {
     
     @State private var selectedIndex: Int = 0
     
+    let columns = [
+        GridItem(.flexible(), spacing: 16),
+        GridItem(.flexible(), spacing: 16)
+    ]
+    
     var body: some View {
         ScrollView {
             GenreListView(selectedIndex: $selectedIndex)
+            
+            LazyVGrid(columns: columns, spacing: 16) {
+                ForEach(HomeConstants.films, id: \.0) { title, year, imageName in
+                    FilmCardView(imageName: imageName, filmTitle: title, fileReleaseYear: year)
+                }
+            }
+            .padding(.horizontal)
         }
     }
 }
