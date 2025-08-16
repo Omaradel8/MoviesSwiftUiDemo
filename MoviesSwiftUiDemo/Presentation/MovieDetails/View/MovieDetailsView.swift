80fe82bfa4154ff3c7980afa2f177d2c78157d94
Omar Adel <omar.adel@softxpert.com>
2025-08-16 17:59:44 +0300

    design movie Footer view

--- MoviesSwiftUiDemo/Presentation/MovieDetails/View/MovieDetailsView.swift
+++ MoviesSwiftUiDemo/Presentation/MovieDetails/View/MovieDetailsView.swift
@@ -22,11 +22,18 @@ struct MovieDetailsView: View {
             // MOVIE HEADER VIEW
             MovieHeaderView(imageName: "poster", title: "Movie Name", genres: "Genres")
             
+            // DESCRIPTION
             Text("\"Lilo & Stitch\" is a Disney movie about a lonely Hawaiian girl named Lilo who adopts a mischievous alien creature she names Stitch, unaware that he's a genetically engineered experiment designed for destruction. The film explores their unlikely friendship as they navigate Lilo's struggles with her older sister and Stitch's attempts to evade capture by intergalactic authorities. Ultimately, the movie emphasizes the importance of family and belonging through the Hawaiian concept of 'ohana'.")
                 .font(.subheadline)
                 .foregroundColor(.white)
                 .padding(.horizontal)
             
+            Spacer(minLength: 100)
+            
+            // FOOTER SECTION
+            MovieFooterView()
+            
+            Spacer(minLength: 50)
         }
         .background(Color.black)
         .ignoresSafeArea()
